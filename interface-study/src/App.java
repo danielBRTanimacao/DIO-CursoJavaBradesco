80f5126018d0325f236a35dd325b358cdf77aeca
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-04-16 13:38:13 -0300

    feat: studying some keywords lambda and interface

--- interface-study/src/App.java
+++ interface-study/src/App.java
@@ -1,5 +1,26 @@
+import java.util.List;
+import java.util.function.Consumer;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        List<User> listOfUsers = List.of(
+            new User("Bico seco", 29, 60.0), 
+            new User("Cururu", 90, 122.1)
+        );
+
+        listOfUsers.forEach((User user) -> {
+            System.out.println(user);
+        }); // Lambda interface funcional
+        
+        // listOfUsers.forEach((User user) -> System.out.println(user));
+        
+        /*var consum = new Consumer<User>() {
+
+            @Override
+            public void accept(final User user) {
+                System.out.println(user);
+            }
+        };
+        listOfUsers.forEach(consum);*/
     }
 }
