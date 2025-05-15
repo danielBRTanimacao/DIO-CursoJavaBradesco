ebbeda0cb1c1607311edebab54dbb1d3b7d307b2
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-05-15 16:58:15 -0300

    feat: updating sudoku game

--- SudokuGame/src/Main.java
+++ SudokuGame/src/Main.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
 public class Main {
+    private final static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
