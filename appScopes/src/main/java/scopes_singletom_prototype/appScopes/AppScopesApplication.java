7eb2de54d2cab7ef35648e1d4884ab653ba6ecc2
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-05-25 17:15:15 -0300

    feat: Studing about scops

---
+++ appScopes/src/main/java/scopes_singletom_prototype/appScopes/AppScopesApplication.java
@@ -0,0 +1,13 @@
+package scopes_singletom_prototype.appScopes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppScopesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppScopesApplication.class, args);
+	}
+
+}
---
+++ appScopes/src/test/java/scopes_singletom_prototype/appScopes/AppScopesApplicationTests.java
@@ -0,0 +1,13 @@
+package scopes_singletom_prototype.appScopes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppScopesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
